1b8b3804d40147dc16bbb5b2bd78da60c253dd1d
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2024-04-14 20:04:01 +0900

    [Perf] デザインの一部修正

--- src/components/Piece/Piece.tsx
+++ src/components/Piece/Piece.tsx
@@ -11,7 +11,7 @@ interface PieceProps {
 
 const Piece: React.FC<PieceProps> = ({ index, status, isInFront, handler, user }) => {
   return (
-    <button onClick={status === 0 ? () => handler(index, user) : () => {}} className={`w-20 h-20 rounded-xl flex items-center justify-center bg-stone-950 border-4 border-amber-400 ${user === 1 ? "hover:border-indigo-500" : "hover:border-rose-500"}`}>
+    <button onClick={status === 0 ? () => handler(index, user) : () => {}} className={`w-20 h-20 max-sm:w-14 max-sm:h-14 max-sm:p-3 rounded-xl flex items-center justify-center bg-stone-950 border-4 border-amber-400 ${user === 1 ? "hover:border-indigo-500" : "hover:border-rose-500"}`}>
       {status === 1 ? <Circle isInFront={isInFront} /> : status === 2 ? <Cross isInFront={isInFront} /> : <></>}
     </button>
   );

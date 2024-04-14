df291e37ffba57c2ef2ffedefcf120966419aab7
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2024-04-14 20:38:28 +0900

    [Perf] Themeの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react'
 import './App.css'
 import Board from './components/Board/Board'
+import { Moon, Sun } from 'react-feather'
 
 function App() {
 
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
+
   return (
-    <>
-      <Board />
-    </>
+    <div className={`w-full h-full grid grid-cols-1 grid-rows-[auto_1fr] justify-items-center ${theme === 'light' ? 'bg-stone-50' : 'bg-stone-950'} `}>
+      <div className="pt-6 flex items-center gap-x-4">
+        <h1 className={`font-bold tracking-wider ${theme === 'light' ? 'text-stone-950' : 'text-stone-50'}`}>Tic Tac Toe!</h1>
+        <button onClick={toggleTheme} className={`p-3 ${theme === 'light' ? 'bg-slate-600/[0.12]' : 'bg-stone-700'}`}>
+          {
+            theme === "light" ?
+            <Sun className="w-6 h-6 text-rose-400" /> :
+            <Moon className="w-6 h-6 text-yellow-400" />
+          }
+        </button>
+      </div>
+      <Board theme={theme} />
+    </div>
   )
 }
 

e68ba2821900c4bed026831abfdad49ce7b363fd
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2024-04-14 21:04:55 +0900

    [Feat] 背景との統一

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   }
 
   return (
-    <div className={`w-full h-full grid grid-cols-1 grid-rows-[auto_1fr] justify-items-center ${theme === 'light' ? 'bg-stone-50' : 'bg-stone-950'} `}>
+    <div className={`w-full h-full grid grid-cols-1 grid-rows-[auto_1fr] justify-items-center ${theme === 'light' ? 'bg-stone-50' : 'bg-stone-950'}`}>
       <div className="pt-6 flex items-center gap-x-4">
         <h1 className={`font-bold tracking-wider ${theme === 'light' ? 'text-stone-950' : 'text-stone-50'}`}>Tic Tac Toe!</h1>
         <button onClick={toggleTheme} className={`p-3 ${theme === 'light' ? 'bg-slate-600/[0.12]' : 'bg-stone-700'}`}>
